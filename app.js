76c0936c2ffb59caf59091fe46d46ae6128869f3
manonthemat <empower@manonthemat.com>
2014-12-03 12:29:07 -0800

    AWS S3

--- app.js
+++ app.js
@@ -2,8 +2,23 @@ var app = require('http').createServer(handler);
 var io = require('socket.io')(app);
 var fs = require('fs');
 var env = process.env.NODE_ENV || 'development';
+var aws_config = require(__dirname + '/config/aws.js')[env];
 var twilio_config = require(__dirname + '/config/twilio.js')[env];
 var twilio_client = require('twilio')(twilio_config.accountSid, twilio_config.authToken);
+
+var AWS = require('aws-sdk');
+AWS.config.accessKeyId = aws_config.AWS_ACCESS_KEY_ID;
+AWS.config.secretAccessKey = aws_config.AWS_SECRET_ACCESS_KEY;
+AWS.config.region = aws_config.region;
+
+var s3bucket = new AWS.S3({params: {Bucket: aws_config.bucket}});
+
+function pushToBucket(data) {
+  s3bucket.putObject(data, function(err, data) {
+    if(err) console.log("An error occured while trying to push to S3 bucket\n", err);
+    else console.log("push to s3 successful");
+  });
+}
 
 function handler(req, resp) {
   var url;
@@ -39,6 +54,7 @@ io.on('connection', function(socket) {
   socket.on('share image', function(data) {
     console.log('a user wants to share an image');
     console.log(data);
+    pushToBucket({Key: data.filename, Body: data.filename});
     sendText(twilio_config.default_recipient, "Your love life will be... interesting.");
   });
 });
---
+++ config/aws.js
@@ -0,0 +1,16 @@
+var aws_config = {
+  development: {
+    AWS_ACCESS_KEY_ID: "",
+    AWS_SECRET_ACCESS_KEY: "",
+    region: "",
+    bucket: ""
+  },
+  production: {
+    AWS_ACCESS_KEY_ID: "",
+    AWS_SECRET_ACCESS_KEY: "",
+    region: "",
+    bucket: ""
+  }
+};
+
+module.exports = aws_config;
